dc4753ca67812bbc10a119e1e3afb3d9fbc86449
katamander <katamander@gmail.com>
2026-09-18 10:54:08 +0200

    workshop 3 halfway

--- git_setup.sh
+++
@@ -1,4 +0,0 @@
-#this is useless!
-#!/bin/bash
-git config --global user.name "katamander"
-git config --global user.email katamander@gmail.com 
---
+++ setup.sh
@@ -0,0 +1,11 @@
+# create virtual environment
+#python -m venv env
+
+# initialise virtual environment
+#source ./env/bin/activate
+
+# install requirements
+pip install -r requirements.txt
+
+# deactivate virtual environment
+#deactivate
